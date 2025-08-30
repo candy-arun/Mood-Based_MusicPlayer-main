74f17873cacc3f8fa2c6e1e7dff3556e6e6f2807
Arun kumar <145600109+candy-arun@users.noreply.github.com>
2025-08-31 02:06:34 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Mood-Based_MusicPlayer-main/",
+  base: '/Mood-Based_MusicPlayer-main/', // <-- repo name as base
 })
